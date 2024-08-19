dbfbe395529f51a005eec26788c7038f0b3a068c
طاها یساقی <softdev1@securitywave.co>
2024-08-19 09:48:54 +0330

    REDSERVER-124 impelement base structure

---
+++ Domain/Entities/Users.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace infrastructure_backend_authenticate.Domain.Entities
+{
+    public class Users
+    {
+        
+    }
+}
