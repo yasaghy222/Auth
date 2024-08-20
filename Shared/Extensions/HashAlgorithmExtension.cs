f70d39ca3b93ff30c12ef8a4d3093b0cbf283aa5
طاها یساقی <softdev1@securitywave.co>
2024-08-20 23:03:23 +0330

    fix hash algoritm impelementation with milan video

--- Shared/Extensions/HashAlgorithmExtension.cs
+++ Shared/Extensions/HashAlgorithmExtension.cs
@@ -5,42 +5,27 @@ namespace Auth.Shared.Extensions
     public static class HashAlgorithmExtension
     {
         private const int SaltSize = 16;
-        private const int KeySize = 32;
+        private const int HashSize = 32;
         private const int Iterations = 100000;
 
         private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA3_512;
 
         public static string ToHashString(this string str)
         {
-            byte[] salt = RandomNumberGenerator.GetBytes();
-            RandomNumberGenerator.Fill(salt);
+            byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(str, salt, Iterations, Algorithm, HashSize);
 
-            using Rfc2898DeriveBytes pbkdf2 = new(str, salt, Iterations, HashAlgorithmName.SHA256);
-            byte[] key = pbkdf2.GetBytes(KeySize);
-            byte[] hash = new byte[SaltSize + KeySize];
-            Array.Copy(salt, 0, hash, 0, SaltSize);
-            Array.Copy(key, 0, hash, SaltSize, KeySize);
-
-            return Convert.ToBase64String(hash);
+            return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
         }
 
         public static bool VerifyHashString(this string hashedStr, string str)
         {
-            byte[] hashBytes = Convert.FromBase64String(hashedStr);
-            byte[] salt = new byte[SaltSize];
-            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+            string[] parts = hashedStr.Split('-');
+            byte[] hash = Convert.FromHexString(parts[0]);
+            byte[] salt = Convert.FromHexString(parts[1]);
+            byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(str, salt, Iterations, Algorithm, HashSize);
 
-            using Rfc2898DeriveBytes pbkdf2 = new(str, salt, Iterations, HashAlgorithmName.SHA256);
-            byte[] key = pbkdf2.GetBytes(KeySize);
-            for (int i = 0; i < KeySize; i++)
-            {
-                if (hashBytes[i + SaltSize] != key[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return CryptographicOperations.FixedTimeEquals(hash, inputHash);
         }
     }
 }
